2669ef8e2bf5dc086b964787aa5762ffde50d205
Gal-Blecher <80682875+Gal-Blecher@users.noreply.github.com>
2023-07-01 16:32:45 +0300

    .

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 setup = {'batch_size': 128,
          'dataset_name': 'cifar10',
-         'experiment_name': 'test_exp',
-         'lr': 1e-3,
+         'experiment_name': 'vae_2',
+         'lr': 1e-2,
          'n_epochs': 50
          }
 
